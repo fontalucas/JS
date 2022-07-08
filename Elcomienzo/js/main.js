f24b683537379678593026884a886474f655beee
fontalucas <fontanellazlucas@gmail.com>
2022-07-07 23:52:17 -0300

    CICLO FOR

--- Elcomienzo/js/main.js
+++ Elcomienzo/js/main.js
@@ -1,37 +1,22 @@
-/* let libro; 
-libro = "Mi libro";
-let hoja = "hoja en blanco";
+let numero = parseInt(prompt("Ingrese un numero"));
 
+for (let i = 1; i <=10; i++){
+    let multi = numero * i;
+    console.log(numero + " x " + i + " = " + multi);   
+}
 
-console.log(libro);
+let pedidos = 0;
+while(cantidadDePedidos <= 15){
+    let cantidadDePedidos = parseInt(prompt("Ingrese su numero de pedido")); 
+    pedidos++;}
+    
+    if(cantidadDePedidos < 15){
+        console.log("Su pedido esta confirmado");}
+    else{
+        console.log("Se ha alcanzado el limite de pedidos");
+    }
 
-const dia = "jueves";
-console.log(dia);
-
-let mensaje1 = "hola";
-let mensaje2 = "como estas";
-console.log(mensaje1 + mensaje2); */
-
-/* let nombre = prompt("Escribi tu nombre");
-console.log(nombre);
-
-let apellido = prompt("Decime tu apellido")
-alert(apellido)
-
-
-const numero1 = parseInt(prompt("ingresa un numero"))
-let numero2 = prompt("ingrese un numero de 2 cifras") //esto devuelve un string //
-numero2 = parseInt(numero2)
-
-console.log(numero1 + numero2) */
-
-
-/* let nombreUsuario = prompt("ingrese su nombre de usuario")
-let contraseña = prompt("ingrese su contraseña")
-let salida = "Bienvenido" + " " + nombreUsuario
-console.log(salida)
-alert(salida) */
-
+/*
 const edadPersona = 19;
 const dineroEnElBolsillo = 2000;
 if(edadPersona >= 18 && dineroEnElBolsillo >= 2000){
@@ -41,25 +26,25 @@ if(edadPersona >= 18 && dineroEnElBolsillo >= 2000){
     }
 }
 else {
-    console.log("No puedes entrar, eres menos")
+    console.log("No puedes entrar, eres menor")
 
-}
-const listado = /* ["valor1", "valor2", "valor3"] */ [5, 7, 3];
+} */
+//const  listado = /* ["valor1", "valor2", "valor3"] */ [5, 7, 3];
 
-listado[2] /* seria el 3 */
-listado[0] /* Seria el 5 */
-listado[posicion]   /* seria el 7 */
+//listado[2] /* seria el 3 */
+//listado[0] /* Seria el 5 */
+//listado[1] /* seria el 7 */
 
-let sumadorDeNotas = 0 ;
+/* let sumadorDeNotas = 0 ;
 
 for(let posicion = 0; posicion < 3; posicion++) {
-    sumadorDeNotas = sumadorDeNotas + listado[posicion];    
+    sumadorDeNotas = sumadorDeNotas + listado[];    
     
 }   
 const promedio = sumadorDeNotas/3; 
-console.log(promedio); /* es igual a 5 */
+console.log(promedio); */ /* es igual a 5 */
 
-if(promedio > 6) {
+/* if(promedio > 6) {
     console.log("Aprobaste, te sacaste un " + promedio);}
     else {
         console.log("No aprobaste, tu nota fue un " + promedio);  
@@ -84,3 +69,18 @@ agregarAlCarrito("Rey Arturo");
 agregarAlCarrito("Maxima");
 agregarAlCarrito("William");
 agregarAlCarrito("Magnus");
+ */
+/* 
+const producto = Burger;
+function Burger(nombreParametro, stockParametro, precio) { // Funcion constructora
+    this.nombreParametro = "";
+    this.stockParametro = 10;
+    this.precio = 800;
+
+} */
+
+/* const reyarturo = new Burger("Rey Arturo", 50, 850);
+const maxima= new Burger("Maxima", 15, 950);
+const burger3 = new Burger();
+const burger4 = new Burger();
+const burger5 = new Burger(); */
